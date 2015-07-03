0e608327bfc2390ffac4e18d8435aede6f207538
barriosuna <osuna.luciana.lo@gmail.com>
2015-07-03 19:22:02 -0300

    Create pythonprograman.py

---
+++ pythonprograman.py
@@ -0,0 +1,21 @@
+
+class Celda:
+	def __init__(self,pos):
+		self.pos = pos
+		
+	def set_Padre_izq(self):
+	def set_Padre_der(self):
+	def set_Hijo_izq(self):
+	def set_Hijo_der(self):
+	def calcular(self):
+	def nivel(self):
+	
+	
+class Piramide:
+  def __init__:
+  
+  def for:
+	#ciclo con las pasadas
+	
+#Declarar las celdas
+Celda=
